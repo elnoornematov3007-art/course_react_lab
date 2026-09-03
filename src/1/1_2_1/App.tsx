04af44c1bebc7c90b1502a88b02b6dec177a78f3
ElnoorNematov <323484825+elnoornematov3007-art@users.noreply.github.com>
2026-09-03 16:12:44 +0600

    1_2_1

--- src/1/1_2_1/App.tsx
+++ src/1/1_2_1/App.tsx
@@ -8,12 +8,14 @@
 
 */
 
-import Gallery, { Profile } from "./Gallery";
+import Profile from './Profile.tsx';
+import Gallery from './Gallery.tsx';
 
 export default function App() {
   return (
     <div>
-      <Profile />      
+      <Profile />
+      <Gallery />      
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './1/1_1_4/style.css'
-import App from './1/1_1_4/App.tsx'
+import './1/1_2_1/style.css'
+import App from './1/1_2_1/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
