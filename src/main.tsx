7d90fcfb7236cce9ea7618fc4310b1f76998949c
ElnoorNematov <323484825+elnoornematov3007-art@users.noreply.github.com>
2026-09-03 14:56:53 +0600

    1_1_1

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './4/4_6_1/style.css'
-import App from './4/4_6_1/App.tsx'
+import './1/1_1_1/style.css'
+import App from './1/1_1_1/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
