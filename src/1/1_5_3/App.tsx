15b727f998975d1117b7ed317ff7176673b60983
ElnoorNematov <323484825+elnoornematov3007-art@users.noreply.github.com>
2026-09-04 15:25:47 +0600

    1_5_3

--- src/1/1_5_3/App.tsx
+++ src/1/1_5_3/App.tsx
@@ -3,27 +3,39 @@
   Извлеките компонент Card из приведенной ниже разметки и используйте параметр children для передачи ему различных JSX.
 */
 
+import { ReactNode } from "react";
+
+function Card({ children }: { children: ReactNode }) {
+    return (
+        <div className="card">
+            <div className="card-content">
+                {children}
+            </div>
+        </div>
+    );
+}
+
 export default function Profile() {
-  return (
-    <div>
-      <div className="card">
-        <div className="card-content">
-          <h1>Photo</h1>
-          <img
-            className="avatar"
-            src="OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
+    return (
+        <div>
+            <Card>
+                <h1>Photo</h1>
+                <img
+                    className="avatar"
+                    src="OKS67lhm.jpg"
+                    alt="Aklilu Lemma"
+                    width={70}
+                    height={70}
+                />
+            </Card>
+
+            <Card>
+                <h1>About</h1>
+                <p>
+                    Aklilu Lemma was a distinguished Ethiopian scientist who
+                    discovered a natural treatment to schistosomiasis.
+                </p>
+            </Card>
         </div>
-      </div>
-      <div className="card">
-        <div className="card-content">
-          <h1>About</h1>
-          <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
